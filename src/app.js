a6e0db25ba95b3adb9a27fe2f485d6c914169b8c
Alex Walters <alexnwalters@gmail.com>
2019-09-13 14:49:27 -0400

    config cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config')
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 
@@ -12,14 +13,18 @@ const morganOption = (NODE_ENV === 'production')
     : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 app.use(helmet());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.json({ok: true});
 })
 
-app.use(function errorHandler(error, req, res, next) {
+app.use((error, req, res, next) => {
     let response
     if (NODE_ENV === 'production') {
         response = { error: { message: 'server error'}}
